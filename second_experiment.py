12fb9c03a2da30a2dfad2e6428f983dd68c6998f
Cristian Sanchez <cristian.yebra@gmail.com>
2024-02-03 16:15:47 +0100

    add second_experiment.py

--- second_experiment.py
+++ second_experiment.py
@@ -1,20 +1,70 @@
-import os
-import torch
-import transformers
-from transformers import (
-  AutoTokenizer,
-  AutoModelForCausalLM,
-  BitsAndBytesConfig
-  pipeline
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import LlamaCpp
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
+prompt_template = """Use the following pieces of context to answer the question at the end. \
+If you don't know the answer, just say that you don't know, don't try to make up an answer. \
+Use 5 lines for each answer.
+
+Context: {context}
+
+Question: {question}
+Helpful Answer:"""
+
+prompt = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
 )
 
-model_name="/home/cristian/development/ai/models/dolphin-2.7-mixtral-8x7b.Q4_K_M.gguf"
+# Callbacks support token-wise streaming
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
-model_config = transformers.AutoConfig.from_pretrained(
-   model_name,
+n_gpu_layers = 8  # The number of layers to put on the GPU. The rest will be on the CPU. If you don't know how many layers there are, you can use -1 to move all to GPU.
+n_batch = 512  # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
+
+# Make sure the model path is correct for your system!
+llm = LlamaCpp(
+    model_path="/home/cristian/development/ai/models/dolphin-2.7-mixtral-8x7b.Q4_K_M.gguf",
+    n_gpu_layers=n_gpu_layers,
+    n_batch=n_batch,
+    n_ctxt=2048,
+    callback_manager=callback_manager,
+    verbose=True,  # Verbose is required to pass to the callback manager
 )
 
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right"
+# llm_chain = LLMChain(prompt=prompt, llm=llm)
+# question = "How will you clean my dirty feet?"
+# llm_chain.run(question)
 
+# Load PDF
+loader = PyPDFLoader("./pdf/Thesis.pdf")
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+# Retrieve and generate using the relevant snippets of the blog.
+retriever = vectorstore.as_retriever()
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | custom_prompt
+    | llm
+    | StrOutputParser()
+)
+
+rag_chain.invoke("Explain extensively the SWAN working modes.")
+
+# cleanup
+vectorstore.delete_collection()
