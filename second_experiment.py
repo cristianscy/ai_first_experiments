48d7101fc29f24b6d5d3fe8f4056130e86d96c3f
Cristian Sanchez <cristian.yebra@gmail.com>
2024-02-03 16:28:22 +0100

    modify embeddings on second_experiment.py

--- second_experiment.py
+++ second_experiment.py
@@ -10,6 +10,8 @@ from langchain_community.vectorstores import Chroma
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 
+from langchain_community.embeddings import LlamaCppEmbeddings
+
 prompt_template = """Use the following pieces of context to answer the question at the end. \
 If you don't know the answer, just say that you don't know, don't try to make up an answer. \
 Use 5 lines for each answer.
@@ -26,12 +28,14 @@ prompt = PromptTemplate(
 # Callbacks support token-wise streaming
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
+llama_model_path="/home/cristian/development/ai/models/dolphin-2.7-mixtral-8x7b.Q4_K_M.gguf"
+
 n_gpu_layers = 8  # The number of layers to put on the GPU. The rest will be on the CPU. If you don't know how many layers there are, you can use -1 to move all to GPU.
 n_batch = 512  # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
 
 # Make sure the model path is correct for your system!
 llm = LlamaCpp(
-    model_path="/home/cristian/development/ai/models/dolphin-2.7-mixtral-8x7b.Q4_K_M.gguf",
+    model_path=llama_model_path,
     n_gpu_layers=n_gpu_layers,
     n_batch=n_batch,
     n_ctxt=2048,
@@ -49,7 +53,8 @@ docs = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 splits = text_splitter.split_documents(docs)
-vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+embeddings = LlamaCppEmbeddings(model_path=llama_model_path, n_ctx=2048)
+vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
 
 # Retrieve and generate using the relevant snippets of the blog.
 retriever = vectorstore.as_retriever()
